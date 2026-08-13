fd65877708637730979d1d3dd087c4eb86005e43
Claudimar Bilis <claudimarbilisdev@gmail.com>
2026-08-12 22:10:38 -0300

    Amend

---
+++ src/simpleExercises/If_Else/AprovadoOuReprovado.java
@@ -0,0 +1,22 @@
+//Peça a nota de um aluno.
+//Se a nota for maior ou igual a 7, informe que foi aprovado.
+//Caso contrário, reprovado.
+
+package simpleExercises.If_Else;
+import java.util.Scanner;
+
+public class AprovadoOuReprovado {
+    public static void main (String[]args){
+        Scanner teclado = new Scanner (System.in);
+
+        System.out.println("Digite sua nota de 0.0 a 10.0: ");
+
+        double nota = teclado.nextDouble();
+
+        if (nota >= 7.0){
+            System.out.println("Parabéns! Você foi aprovado!");
+        } else {
+            System.out.println("Que pena! Você foi reprovado!");
+        }
+    }
+}
---
+++ src/simpleExercises/Variaveis_e_Scanner/Media.java
@@ -0,0 +1,22 @@
+//Peça três notas.
+//Calcule e mostre a média.
+
+package simpleExercises.Variaveis_e_Scanner;
+import java.util.Scanner;
+
+public class Media {
+    public static void main (String[]args){
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite sua 1ª, 2ª e 3ª nota da disciplina: ");
+
+        double primeiraNota = teclado.nextDouble();
+        double segundaNota = teclado.nextDouble();
+        double terceiraNota = teclado.nextDouble();
+
+        double media = (primeiraNota + segundaNota + terceiraNota) / 3;
+
+        System.out.println("A média das suas notas é: " + media);
+    }
+}
